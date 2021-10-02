b6f182ce6f1a0902c4634736332abbd6ce132995
Andrew Rusli <andreworusli@gmail.com>
2021-10-02 10:12:59 -0400

    add contentful-gatsby plugin, connected successfully

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,11 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+// node snippet to access .env files
+ require("dotenv").config({
+   path: `.env.${process.env.NODE_ENV}`,
+ })
+
 module.exports = {
   /* Your site config here */
   siteMetadata: {
@@ -11,5 +16,14 @@ module.exports = {
     description: 'Portfolio for Piyali Sircar',
     author: 'Piyali Sircar',
   },
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `d1xnaae74je6`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_API_KEY,
+      },
+    },
+  ], // end plugins array
+} // end module.exports object
