79e2675b2711aa1062ed7ce2b501284990eb40f3
Andrew Rusli <andreworusli@gmail.com>
2021-10-04 16:32:57 -0400

    edit main.css and added styled component to navbar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import '../assets/styles/main.css'
 
 const Layout = ({children}) => {
     return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import {Link} from 'gatsby'
+import styled from 'styled-components'
 
 const Navbar = () => {
     return (
-        <div>
+        <Wrapper>
             <h1>Piyali Sircar - <Link to='/'>Work</Link> | <Link to='/about'>About</Link></h1>
-        </div>
+        </Wrapper>
     )
 }
+
+//styled components
+const Wrapper = styled.div`
+    background-color: lightblue;
+`
 
 export default Navbar
